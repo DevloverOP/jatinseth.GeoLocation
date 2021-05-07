4f8c524c17867ad77ee7e0150678a3c9573591de
Jatin Seth <79426741+JatinSeth@users.noreply.github.com>
2021-05-08 01:10:26 +0530

    Update script.js
    upd

--- script.js
+++ script.js
@@ -9,26 +9,49 @@ let prevcords={};
 //getting coordinates
 
 function yourLocation(pos) {
-  document.getElementById("indicator").style.backgroundColor="green";
-  document.getElementById("lat").innerHTML = pos.coords.latitude;
+document.getElementById("indicator").style.backgroundColor="green";
+ document.getElementById("lat").innerHTML = pos.coords.latitude;
   document.getElementById("long").innerHTML = pos.coords.longitude;
   loc.innerHTML = "Accuracy:-" + pos.coords.accuracy;
   console.log(prevcords);
-if(prevcords.lat===pos.coords.latitude && prevcords.lon===pos.coords.longitude){ console.log(prevcords.lat,pos.coords.latitude);
-
+if(prevcords.lat===pos.coords.latitude && prevcords.lon===pos.coords.longitude){
   document.getElementById("speed").innerHTML = `Speed:${0}`;
   document.getElementById("state").innerHTML = "Stopped";
   document.getElementById("state").style = deco("stop");
-}
-else{
-  console.log(prevcords.lat,pos.coords.latitude);
+}else{
   prevcords.lat=pos.coords.latitude;
   prevcords.lon=pos.coords.longitude;
-  document.getElementById("speed").innerHTML = `Speed:${pos.coords.spee?pos.coords.spee:0}`;
+  document.getElementById("speed").innerHTML = `Speed:${pos.coords.speed}`;
   document.getElementById("state").innerHTML = "Moving";
   document.getElementById("state").style = deco("move");
 }
 
+  // prevcords={
+  //   lat:pos.coords.latitude,
+  //   lon:pos.coords.longitude
+  // }
+  
+  
+
+
+
+
+
+
+
+
+  // let speed =0;
+  // if (speed===pos.coords.speed||pos.coords.speed === null) {
+  //   console.log(speed,pos.coords.speed);
+  //   document.getElementById("speed").innerHTML = `Speed:${0}`;
+  //   document.getElementById("state").innerHTML = "Stopped";
+  //   document.getElementById("state").style = deco("stop");
+  // } else{
+  //   speed = pos.coords.speed;
+  //   document.getElementById("speed").innerHTML = `Speed:${speed}`;
+  //   document.getElementById("state").innerHTML = "Moving";
+  //   document.getElementById("state").style = deco("move");
+  // }
   setTimeout(() => {
     document.getElementById("indicator").style.backgroundColor="red";  
   },1000); 
@@ -67,6 +90,11 @@ function catchError(err) {
 //clearing labels
 document.getElementById("clear").addEventListener("click", () => {
   location.reload();
+  // document.getElementById("lat").innerHTML = "Latitude";
+  // document.getElementById("long").innerHTML = "Longitude";
+  // document.getElementById("state").style = deco("stop");
+  // loc.innerHTML = "Accuracy:-0";
+  // document.getElementById("indicator").style.backgroundColor="red"; 
 });
 
 
